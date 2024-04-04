4e68efb04897be554b1a1af7bf7f3d8e54798118
TomMaullin <tommaullin@gmail.com>
2024-04-04 12:44:04 +0100

    09 underway

--- src/preproc/filter_columns_by_site.py
+++ src/preproc/filter_columns_by_site.py
@@ -1,21 +1,35 @@
+from src.memmap.MemoryMappedDF import MemoryMappedDF
+
 # ------------------------------------------------------------
 # Filters columns based on the specified site number.
 #
 # Parameters:
-# - df: pandas.DataFrame to filter.
+# - df: pandas.DataFrame or Memory Mapped Dataframe to filter.
 # - site_number: The site number to filter columns by.
+# - return_df: If true the filtered dataframe is returned, else
+#              just the list of columns
 #
 # Returns:
 # - A DataFrame with only the columns that match the specified
-#   site number.
+#   site number, or just the column headers for the dataframe.
 # ------------------------------------------------------------
-def filter_columns_by_site(df, site_number):
+def filter_columns_by_site(df, site_number, return_df=True):
     
     # Construct the site string to match in column names
-    site_str = f"_Site_{site_number}"
+    site_str = f"Site_{site_number}"
     
     # Filter columns that contain the site string
     filtered_columns = [col for col in df.columns if site_str in col]
     
     # Return the DataFrame with only the filtered columns
-    return df[filtered_columns]
+    if return_df:
+
+        # Check the type of the dataframe to return
+        if type(df)==MemoryMappedDF:
+            return(df[:,filtered_columns])
+        else:
+            return(df[filtered_columns])
+
+    # Otherwise return just column names
+    else:
+        return(filtered_columns)
